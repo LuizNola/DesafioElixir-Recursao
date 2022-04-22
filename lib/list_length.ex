0ca561c94f583a22a8bb46e6181fe25354f92442
Luiz Nola <luizn647@gmail.com>
2022-04-22 09:49:00 -0300

    feat: resolvendo o desafio

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    listLength(list, 0)
+  end
+
+  defp listLength([], acc) do
+    acc
+  end
+
+  defp listLength([head | tail] = list, acc) do
+    acc = acc + 1
+    listLength(tail, acc)
   end
 end
